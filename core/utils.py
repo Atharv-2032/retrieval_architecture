e38c9b6c0308d8be1e0c1609fe78d14a304ecd1a
IshanKO9 <ishankelkar09@gmail.com>
2026-04-28 16:48:36 +0400

    fixed - matching error

--- core/utils.py
+++ core/utils.py
@@ -23,6 +23,6 @@ def extract_json(text):
 def normalize_entity(entity):
     text = entity.lower()
     text = re.sub(r"\(.*?\)", "", text)       
-    text = re.sub(r"[^a-z0-9\s]", " ", text)          
+    text = re.sub(r"[^a-z0-9\s\-]", " ", text)          
     text = re.sub(r"\s+", " ", text)          
     return text.strip()
